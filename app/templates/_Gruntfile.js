e6fb68c65857db29a39f8387bb8ddd05ee3731e9
Carl Topham <carl@carl-topham.com>
2014-05-08 18:21:48 +0100

    whitespace and indentations
    and a fix for the js watch!

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -1,164 +1,115 @@
 /* Generated for <%= site_name %> */
-
-module.exports = function(grunt) {
-
-    require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
-    
-    // 1. All configuration goes here
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-
-
-        //watch
-
-
-        watch: {
-          css: {
-            files: '../dev/src/scss/**/*.scss',
-            tasks: ['sass:dev', 'autoprefixer', 'notify:sassDone' ],
-            options: {
-              livereload: true,
-            },
-          },
-
-          html: {
-            files: '../dev/**/*.html',
-            options: {
-              livereload: true,
-            },
-          },
-
-          js: {
-            files: '../dev/static/js/**/*.html',
-            options: {
-              livereload: true,
-            },
-          },
-
-          jade: {
-            files: '../dev/src/jade/**/*.jade',
-            tasks: ['jade' ],
-            options: {
-              livereload: true,
-            },
-          },
-
+module.exports = function (grunt) {
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+  // 1. All configuration goes here
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    //watch
+    watch: {
+      css: {
+        files: '../dev/src/scss/**/*.scss',
+        tasks: ['sass:dev', 'autoprefixer', 'notify:sassDone'],
+        options: {
+          livereload: true,
         },
-
-
-        // end watch
-
-        //sass
-
-
-
-        sass: {                              // Task
-            dev: {                            // Target
-              options: {                       // Target options
-                style: 'expanded',
-                sourcemap: true,
-              },
-              files: {                         // Dictionary of files
-                '../dev/static/css/style.css': '../dev/src/scss/style.scss',
-              }
-            },
-            dist: {                            // Target
-              options: {                       // Target options
-                style: 'expanded',
-              },
-              files: {                         // Dictionary of files
-                '../dev/static/css/style.css': '../dev/src/scss/style.scss',
-              }
-            }
-          },
-
-
-
-        // end sass
-
-
-
-        //jade
-
-        jade: {
-          compile: {
-              options: {
-                  client: false,
-                  pretty: true
-              },
-              files: [ {
-                cwd: "../dev/src/jade/",
-                src: ['**/*.jade', '!**/_*.jade'], //ignore all _jade files
-                dest: "../dev",
-                expand: true,
-                ext: ".html"
-              } ]
-          }
+      },
+      html: {
+        files: '../dev/**/*.html',
+        options: {
+          livereload: true,
         },
-
-        //end jade
-
-
-
-        //auto prefixer
-
-
-        autoprefixer: {
-
-            options: {
-              browsers: ['last 8 version', 'ie 8', 'ie 7']
-            },
-
-            // just prefix the specified file
-            single_file: {
-              options: {
-                // Target-specific options go here.
-              },
-              src: '../dev/static/css/style.css',
-              dest: '../dev/static/css/style.css'
-            }
-          },
-
-
-       //end auto prefixer
-
-
-      //css min
-
-      cssmin: {
-        minify: {
-          expand: true,
-          cwd: '../dev/static/css',
-          src: ['*.css', '!*.min.css'],
-          dest: '../dist/static/css',
-          ext: '.css',
-          report: 'gzip'
+      },
+      js: {
+        files: '../dev/static/js/**/*.js',
+        options: {
+          livereload: true,
+        },
+      },
+      jade: {
+        files: '../dev/src/jade/**/*.jade',
+        tasks: ['jade'],
+        options: {
+          livereload: true,
+        },
+      },
+    },
+    // end watch
+    //sass
+    sass: { // Task
+      dev: { // Target
+        options: { // Target options
+          style: 'expanded',
+          sourcemap: true,
+        },
+        files: { // Dictionary of files
+          '../dev/static/css/style.css': '../dev/src/scss/style.scss',
         }
       },
-
-      // end ccs min
-
-
-
-
-      //check using csscss - need right version of ruby!
-      csscss: {
-          options: {
-            minMatch: 4,
-            verbose: true,
-            shorthand: false,
-          },
-          dist: {
-            src: ['../dev/static/css/style.css']
-          }
+      dist: { // Target
+        options: { // Target options
+          style: 'expanded',
         },
-
-
-
-
-
-
-
+        files: { // Dictionary of files
+          '../dev/static/css/style.css': '../dev/src/scss/style.scss',
+        }
+      }
+    },
+    // end sass
+    //jade
+    jade: {
+      compile: {
+        options: {
+          client: false,
+          pretty: true
+        },
+        files: [{
+          cwd: "../dev/src/jade/",
+          src: ['**/*.jade', '!**/_*.jade'], //ignore all _jade files
+          dest: "../dev",
+          expand: true,
+          ext: ".html"
+        }]
+      }
+    },
+    //end jade
+    //auto prefixer
+    autoprefixer: {
+      options: {
+        browsers: ['last 8 version', 'ie 8', 'ie 7']
+      },
+      // just prefix the specified file
+      single_file: {
+        options: {
+          // Target-specific options go here.
+        },
+        src: '../dev/static/css/style.css',
+        dest: '../dev/static/css/style.css'
+      }
+    },
+    //end auto prefixer
+    //css min
+    cssmin: {
+      minify: {
+        expand: true,
+        cwd: '../dev/static/css',
+        src: ['*.css', '!*.min.css'],
+        dest: '../dist/static/css',
+        ext: '.css',
+        report: 'gzip'
+      }
+    },
+    // end ccs min
+    //check using csscss - need right version of ruby!
+    csscss: {
+      options: {
+        minMatch: 4,
+        verbose: true,
+        shorthand: false,
+      },
+      dist: {
+        src: ['../dev/static/css/style.css']
+      }
+    },
     //Dist production
     copy: {
       dist: {
@@ -171,73 +122,45 @@ module.exports = function(grunt) {
         }]
       }
     },
-
     'useminPrepare': {
       options: {
         dest: '../dist'
       },
-      //html: '../dev/index.html'
       src: ['../dev/*.html'],
     },
-
     usemin: {
-      //html: ['../dist/index.html']
-      html : '../dist/*.html'
+      html: '../dist/*.html'
     },
     // end Dist production  
-
-   
-
-
-
-
-
-
-
     //notify
     notify: {
       done: {
         options: {
-          title: 'Done!',  // optional
+          title: 'Done!', // optional
           message: 'Whatever you were doing is done!', //required
         }
       },
-
       distStart: {
         options: {
-          title: '🎁 Prepping for distribution!',  // optional
+          title: ' Prepping for distribution!', // optional
           message: 'Get ready for the awesome...', //required
         }
       },
-
       distDone: {
         options: {
-          title: "<%= like_text %>" ,  // optional
-          message: "<%= site_name %> is ready to be distributed 👊", //needed escaping!
+          title: "<%= like_text %>", // optional
+          message: "<%= site_name %> is ready to be distributed ", //needed escaping!
         }
       },
-
-
       sassDone: {
         options: {
-          title: '🎉 Ta-da!!!' ,  // optional
-          message: 'Sass has compiled successfully 😊', //required
+          title: ' Ta-da!!!', // optional
+          message: 'Sass has compiled successfully ', //required
         }
       },
-
-
-
-
-
     }
-
-
-    });//end grunt package configs
-
-    grunt.registerTask('dist', ['notify:distStart', 'jade', 'useminPrepare', 'copy', 'concat', 'uglify', 'usemin', 'sass:dist', 'autoprefixer', 'cssmin', 'notify:distDone']);
-
-
-    // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', []);
-    
+  }); //end grunt package configs
+  grunt.registerTask('dist', ['notifykey: "value",  distStart', 'jade', 'useminPrepare', 'copy', 'concat', 'uglify', 'usemin', 'sass:dist', 'autoprefixer', 'cssmin', 'notify:distDone']);
+  // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
+  grunt.registerTask('default', []);
 };
